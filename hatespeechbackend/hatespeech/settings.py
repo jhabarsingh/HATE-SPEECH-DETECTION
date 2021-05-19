79e7948b3f29b3384ef678f2e464b0f69469d960
jhabarsingh <jhabarsinghbhati23@gmail.com>
2021-05-19 18:25:25 +0530

    Added corsheader in settings.py

--- hatespeechbackend/hatespeech/settings.py
+++ hatespeechbackend/hatespeech/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'hatespeechapi'
+    'hatespeechapi',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
