76f038219d7b34c45605642f5eff11ebe4143f31
jhabarsingh <jhabarsinghbhati23@gmail.com>
2021-05-19 09:01:41 +0530

    Added views

--- hatespeech/hatespeechapi/production.py
+++ hatespeech/hatespeechapi/production.py
@@ -8,22 +8,19 @@ from nltk.corpus import stopwords
 from keras.preprocessing.sequence import pad_sequences
 from tensorflow.keras.preprocessing import text, sequence
 
-if __name__ == '__main__':
-    x_test = ["hi how are you and fuck off"]
-    
-    x_tokenizer = None
-
-    with open("toxic_tokenizer.pkl", "rb") as rfile:
-        x_tokenizer = pickle.load(rfile)
-
-
- 
-    max_text_length = 400
-
-    def joiner(file_name):
+def joiner(file_name):
         paths = os.path.dirname(os.path.abspath(__file__))
         paths = os.path.join(paths, file_name)
         return paths
+
+def predict(text):
+    x_test = [text]
+    
+    x_tokenizer = None
+    with open(joiner("toxic_tokenizer.pkl"), "rb") as rfile:
+        x_tokenizer = pickle.load(rfile)
+
+    max_text_length = 400
 
     model = load_model(joiner('toxic_cnn.h5'))
 
@@ -33,6 +30,6 @@ if __name__ == '__main__':
     y_pred = model.predict(x_testing,verbose=1,batch_size=32)
 
     y_pred = [0 if y[0] < 0.5 else 1 for y in y_pred]
-    print(y_pred)
+    return y_pred[0]
 
 
--- hatespeech/hatespeechapi/views.py
+++ hatespeech/hatespeechapi/views.py
@@ -1,3 +1,36 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .production import predict
 
-# Create your views here.
+class HateSpeechApi(APIView):
+    '''
+        HateSpeech Detection POST APi
+    '''
+
+    def post(self, request):
+        try:
+            text = request.data.get("text", None)
+            if text == None:
+                raise Exception("Invalid text")
+            if len(text.strip()) == 0:
+                raise Exception("Zero Size")
+        except:
+            data = {
+                "message": "invalid Message"
+            }
+            return Response(data)
+
+        text = text.strip()
+        
+        prediction = predict(text)
+        message = "Some error has been occured, Please Try Later"
+
+        if prediction == 0:
+            message = "no"
+        else:
+            message = "yes"
+        data = {
+            "hatespeech": prediction
+        }
+        return Response(data)
