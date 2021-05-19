bd739766121fea1c76e91dcd28aefb7bc94718c3
jhabarsingh <jhabarsinghbhati23@gmail.com>
2021-05-19 17:38:09 +0530

    Added App router

--- hatespeechfrontend/src/App.vue
+++ hatespeechfrontend/src/App.vue
@@ -1,13 +1,17 @@
 <template>
-  
+  <v-app>
+    <v-main>
+      <router-view/>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
 
 export default {
-  
+
   data: () => ({
-  
+
   }),
 };
 </script>
