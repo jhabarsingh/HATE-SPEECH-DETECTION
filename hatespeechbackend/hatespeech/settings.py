233992304bfb1794bce2243aeb7e3312dd723e39
jhabarsingh <jhabarsinghbhati23@gmail.com>
2021-05-19 10:28:39 +0530

    Added staticfiles root

--- hatespeechbackend/hatespeech/settings.py
+++ hatespeechbackend/hatespeech/settings.py
@@ -121,6 +121,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
