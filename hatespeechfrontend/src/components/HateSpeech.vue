0f517fde3e17cba5a8ae7cef9299ac79ac99426b
jhabarsingh <jhabarsinghbhati23@gmail.com>
2021-05-19 17:37:30 +0530

    Added Hatesppech component

---
+++ hatespeechfrontend/src/components/HateSpeech.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-parallax
+    dark
+    src="https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg"
+    style="min-height:100vh;"
+  >
+    <v-row
+      align="center"
+      justify="center"
+    >
+      <v-col class="text-center" cols="12">
+        <HateSpeechChild />
+      </v-col>
+    </v-row>
+  </v-parallax>
+</template>
+
+<script>
+import HateSpeechChild from './HateSpeechChild.vue'
+export default {
+  data: () => ({
+  }),
+  components: {
+    HateSpeechChild
+  }
+}
+</script>
